e872291798732e3a0eac9a2c37d09f4d37bcf3ca
Marvin Thielk <MarvinT@github.com>
2018-09-12 11:03:01 -0700

    initial cookiecutter commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='morphs',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for my morph project and analysis',
+    author='Marvin Thielk',
+    license='MIT',
+)
