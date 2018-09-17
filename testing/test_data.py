ce4677fb760a6ca960a88a7c5fa093efa6efa937
Marvin T <MarvinT@users.noreply.github.com>
2018-09-16 21:53:56 -0400

    assert exists

--- testing/test_data.py
+++ testing/test_data.py
@@ -5,6 +5,7 @@ import morphs
 def test_download_ephys_data():
     target = morphs.paths.DATA_DIR / 'ephys' / 'B1096' / 'kwik' / 'Pen04_Lft_AP2500_ML50__Site02_Z2500__B1096_cat_P04_S02_1'
     target.mkdir(parents=True, exist_ok=True)
-    dest_path = str(target / 'B1096_cat_P04_S02_1.kwik')
+    dest_path = target / 'B1096_cat_P04_S02_1.kwik'
     gdd.download_file_from_google_drive(file_id='12bp8fHCC51PWOiX8QxziY7oM7sOxQetA',
-                                        dest_path=dest_path)
+                                        dest_path=str(dest_path))
+    assert dest_path.exists()
