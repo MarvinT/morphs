ff23f85f5d7d0150dfebb74a9cf24905759627a9
Marvin T <MarvinT@users.noreply.github.com>
2018-09-19 16:59:44 -0700

    test_cluster_accuracy

--- testing/test_accuracies.py
+++ testing/test_accuracies.py
@@ -8,3 +8,24 @@ def test_load_gen_cluster_accuracies():
     assert not morphs.paths.ACCURACIES_PKL.exists()
     accuracies, cluster_accuracies = morphs.data.accuracies.load_cluster_accuracies()
     assert morphs.paths.ACCURACIES_PKL.exists()
+
+@pytest.mark.run(order=2)
+def test_cluster_accuracy():
+    assert len(morphs.paths.blocks()) > 0
+	block_path = morphs.paths.blocks()[]
+	assert morphs.paths.ACCURACIES_PKL.exists()
+	accuracies, cluster_accuracies = morphs.data.accuracies.load_cluster_accuracies()
+	cluster = cluster_accuracies[block_path].index[-1]
+	spikes = morphs.data.load.ephys(block_path, good_clusters=[cluster], collapse_endpoints=True)
+	assert len(spikes['recording'].unique()) >= 1
+	template_spikes = spikes[spikes['stim_id'].isin(list('abcdefgh'))]
+    cluster_groups = template_spikes.groupby('cluster')
+
+    morph_dims = spikes.morph_dim.unique()
+    morph_dims.sort()
+    morph_dims = morph_dims[1:]
+    max_num_reps = np.max([len(stim_group.groupby(by=('recording', 'stim_presentation')))
+                           for stim_id, stim_group in template_spikes.groupby('stim_id')])
+
+    accuracies_list = [cluster_accuracy(cluster, cluster_group, morph_dims, max_num_reps)
+                           for (cluster, cluster_group) in cluster_groups]
