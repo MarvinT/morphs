00d45a49783fcef7e75f4e2034f16487aade6fbb
Marvin T <MarvinT@users.noreply.github.com>
2018-09-19 16:06:24 -0700

    test_load_gen_cluster_accuracies

--- testing/test_accuracies.py
+++ testing/test_accuracies.py
@@ -3,7 +3,8 @@ import pytest
 
 
 @pytest.mark.run(order=3)
-def test_gen_cluster_accuracies():
+def test_load_gen_cluster_accuracies():
     assert len(morphs.paths.blocks()) > 0
-    morphs.data.accuracies.gen_cluster_accuracies()
+    assert not morphs.paths.ACCURACIES_PKL.exists()
+    accuracies, cluster_accuracies = morphs.data.accuracies.load_cluster_accuracies()
     assert morphs.paths.ACCURACIES_PKL.exists()
