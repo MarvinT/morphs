9f3c30194a543690164362ef55cec92f0ac776f5
Marvin T <MarvinT@users.noreply.github.com>
2018-10-09 06:09:02 -0700

    rest of neurometric.py

--- morphs/data/neurometric.py
+++ morphs/data/neurometric.py
@@ -1,8 +1,175 @@
 import pandas as pd
 import numpy as np
+import morphs
 import sklearn as skl
 import sklearn.linear_model
 from sklearn.linear_model import LogisticRegression
+from joblib import Parallel, delayed
+
+
+def hold_one_out_neurometric_fit_dist(representations, labels, behavior_subj, psychometric_params,
+                                      shuffle_count=1024, parallel=True, n_jobs=morphs.parallel.N_JOBS):
+    '''
+    fits behavioral psychometric curves using the representation in a hold one out manner
+
+    Parameters
+    -----
+    representations : np.array
+        size = (num_data_points, num_dimensions)
+    labels : iterable of string labels or np array of dtype='S5'
+        labels : Pandas.DataFrame
+            len = num_data_points
+            required columns = ['morph_dim', 'morph_pos']
+            overwritten/created columns = ['behave_data', 'p_r', 'p_l']
+    behavior_subj : str
+    shuffle_count : int
+    calibrate : boolean
+
+    Returns
+    -----
+    '''
+    label_df = make_label_df(labels, behavior_subj, psychometric_params)
+    behavior_df = make_behavior_df(behavior_subj, psychometric_params)
+
+    if parallel:
+        all_samples = Parallel(n_jobs=n_jobs)(delayed(calc_samples)(representations, label_df, behavior_df,
+                                                                    idx, shuffle=shuffle) for idx, shuffle in [(i, i != 0) for i in xrange(shuffle_count + 1)])
+    else:
+        all_samples = [calc_samples(representations, label_df, behavior_df, idx, shuffle=shuffle)
+                       for idx, shuffle in [(i, i != 0) for i in xrange(shuffle_count + 1)]]
+    all_samples_df = pd.concat(all_samples, ignore_index=True)
+    all_samples_df['subj'] = behavior_subj
+    return all_samples_df
+
+
+def hold_one_out_neurometric_fit_dist_all_subj(representations, labels, psychometric_params,
+                                               shuffle_count=1024, parallel=True, n_jobs=morphs.parallel.N_JOBS):
+    all_samples = []
+    for subj in psychometric_params:
+        print subj
+        all_samples.append(hold_one_out_psychometric_fit_dist(representations, labels, subj, psychometric_params,
+                                                              shuffle_count=shuffle_count, parallel=parallel,
+                                                              n_jobs=n_jobs))
+    return pd.concat(all_samples)
+
+
+def make_label_df(labels, behavior_subj, psychometric_params):
+    label_df = pd.DataFrame(data={'stim_id': labels})
+    m.morph.parse_stim_id(label_df)
+
+    label_df['behave_data'] = False
+    for dim, dim_group in label_df.groupby('morph_dim'):
+        if dim in psychometric_params[behavior_subj]:
+            label_df.loc[dim_group.index, 'behave_data'] = True
+
+    parse_effective_morph(label_df, behavior_subj)
+    return label_df
+
+
+def make_behavior_df(behavior_subj, psychometric_params):
+    morph_dims, morph_poss = zip(
+        *itertools.product(psychometric_params[behavior_subj].keys(), np.arange(1, 129)))
+    behavior_df = pd.DataFrame(data={'morph_dim': morph_dims, 'morph_pos': morph_poss})
+    behavior_df['lesser_dim'] = behavior_df['morph_dim'].str[0]
+    behavior_df['greater_dim'] = behavior_df['morph_dim'].str[1]
+    parse_effective_morph(behavior_df, behavior_subj)
+    for dim, dim_group in behavior_df.groupby('morph_dim'):
+        psyc = m.normalized_four_param_logistic(psychometric_params[behavior_subj][dim])
+        behavior_df.loc[dim_group.index, 'p_greater'] = dim_group['morph_pos'].apply(psyc)
+    behavior_df['p_lesser'] = 1.0 - behavior_df['p_greater']
+    behavior_df['p_left'], behavior_df['p_right'] = behavior_df[
+        'p_lesser'], behavior_df['p_greater']
+    behavior_df.loc[behavior_df['inverted'], 'p_right'] = behavior_df.loc[
+        behavior_df['inverted'], 'p_lesser']
+    behavior_df.loc[behavior_df['inverted'], 'p_left'] = behavior_df.loc[
+        behavior_df['inverted'], 'p_greater']
+    return behavior_df
+
+
+def parse_effective_morph(df, behavior_subj):
+    left, right = m.morph.training[behavior_subj].lower().split('|')
+
+    df['inverted'] = df['lesser_dim'].str.match(in_pattern(right)) & df[
+        'greater_dim'].str.match(in_pattern(left))
+
+    df['effective_dim'] = df['morph_dim']
+    df.loc[df['inverted'], 'effective_dim'] = df['morph_dim'].str[::-1][df['inverted']]
+
+    df['effective_pos'] = df['morph_pos']
+    df.loc[df['inverted'], 'effective_pos'] = 128 - (df[df['inverted']]['morph_pos'] - 1)
+
+
+def in_pattern(string):
+    return r'[' + string + r']'
+
+
+def calc_samples(representations, label_df, behavior_df, idx, shuffle=False, tol=1e-4):
+    error_list, dim_list = fit_held_outs(_merge_df(label_df, behavior_df, shuffle=shuffle),
+                                         representations, tol=tol)
+    return pd.DataFrame(data={'errors': error_list, 'held_out_dim': dim_list, 'shuffle_index': idx, 'shuffled': shuffle})
+
+
+def _merge_df(label_df, behavior_df, shuffle=False):
+    shuffle_effective_dim(label_df, shuffle=shuffle)
+    shuffle_effective_dim(behavior_df, shuffle=False)
+    return pd.merge(label_df, behavior_df[['shuffled_dim', 'effective_pos', 'p_left', 'p_right']],
+                    on=['shuffled_dim', 'effective_pos'], how='left', validate='m:1')
+
+
+def shuffle_effective_dim(df, shuffle=False):
+    if shuffle:
+        behave_dims = df[df['behave_data']]['effective_dim'].unique()
+        non_behave_dims = set(df['effective_dim'].unique()) - set(behave_dims)
+        dim_map = {dim: target for dim, target in zip(
+            behave_dims, np.random.permutation(behave_dims))}
+        dim_map.update({dim: dim for dim in non_behave_dims})
+        df['shuffled_dim'] = df['effective_dim'].map(dim_map)
+    else:
+        df['shuffled_dim'] = df['effective_dim']
+
+
+def fit_held_outs(merged_df, representations, accum='sse', tol=1e-4):
+    mbdf = merged_df[merged_df['behave_data']]
+    error_list = []
+    dim_list = []
+    for held_out_dim in mbdf['shuffled_dim'].unique():
+        training_df = mbdf[mbdf['shuffled_dim'] != held_out_dim]
+        held_out_df = mbdf[mbdf['shuffled_dim'] == held_out_dim]
+        train_x = np.concatenate([representations[training_df.index, :],
+                                  representations[training_df.index, :]])
+        train_y = np.repeat([0, 1], len(training_df))
+        train_weights = np.concatenate([training_df['p_left'], training_df['p_right']])
+
+        test_x = representations[held_out_df.index, :]
+        test_y = held_out_df['p_right']
+
+        model = LogisticRegression(penalty='l2', tol=tol, warm_start=True).fit(
+            train_x, train_y, sample_weight=train_weights)
+        predicted_values = model.predict_proba(test_x)[:, 1]
+
+        dim_list.append(held_out_dim)
+        if accum == 'df':
+            fit_df = held_out_df[['stim_id', 'p_right']].copy()
+            fit_df['predicted'] = predicted_values
+            error_list.append(fit_df)
+        elif accum == 'mse':
+            error_list.append(np.square(predicted_values - test_y).mean())
+        elif accum == 'sse':
+            error_list.append(np.square(predicted_values - test_y).sum())
+        elif accum == 'sigmoid fit':
+            raise NotImplementedError
+        else:
+            raise Exception('invalid accum option')
+    return error_list, dim_list
+
+
+def gen_held_out_df(merged_df, representations, melt=False):
+    held_out_df = pd.concat(fit_held_outs(merged_df, representations, accum='df')[0])
+    if melt:
+        held_out_df = pd.melt(held_out_df, id_vars=['stim_id'], value_vars=['p_right', 'predicted'],
+                              var_name='legend', value_name='p_right')
+    morphs.data.parse.stim_id(held_out_df)
+    return held_out_df
 
 
 def logistic_dim_discriminants(X, labels):
