5d6bda5c37478e64c120cf81a268d491c017adcb
Marvin T <MarvinT@users.noreply.github.com>
2018-12-11 15:25:01 -0800

    import all_loc in load

--- morphs/data/load.py
+++ morphs/data/load.py
@@ -9,6 +9,7 @@ from morphs.data.accuracies import load_cluster_accuracies as cluster_accuracies
 from morphs.data.behavior import load_behavior_df as behavior_df
 from morphs.data.psychometric import load_psychometric_params as psychometric_params
 from morphs.data.neurometric import load_neurometric_null_all as neurometric_null_all
+from morphs.data.localize import load_all_loc as all_loc
 
 
 def ephys_data(block_path, good_clusters=None, collapse_endpoints=False, shuffle_endpoints=False):
