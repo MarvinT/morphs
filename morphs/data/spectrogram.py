ea2ac520a65ed3e9784cc1b5019da90f18fd3673
Marvin T <MarvinT@users.noreply.github.com>
2018-12-16 12:52:38 -0800

    download function call

--- morphs/data/spectrogram.py
+++ morphs/data/spectrogram.py
@@ -4,7 +4,7 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 
 def load_morph_spectrograms():
     if not morphs.paths.SPECT_PKL.exists():
-        download_all_loc()
+        download_morph_spectrograms()
     return morphs.data.load._pickle(morphs.paths.SPECT_PKL.as_posix())
 
 
