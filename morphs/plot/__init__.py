4b4293dd6bd9e5465864b4257e7227913df5cdd6
Marvin T <MarvinT@users.noreply.github.com>
2019-01-23 11:43:02 -0500

    import singleunit

--- morphs/plot/__init__.py
+++ morphs/plot/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import morphs.plot.psychometric
 import morphs.plot.neurometric
+import morphs.plot.singleunit
 import morphs.plot.spectrogram
 from morphs.plot.utils import savefig
