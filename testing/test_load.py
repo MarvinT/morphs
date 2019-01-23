998d945ce5d87ad80655cfefaf3f7ced0363ec37
Marvin T <MarvinT@users.noreply.github.com>
2019-01-23 11:05:19 -0500

    load ephys tests

--- testing/test_load.py
+++ testing/test_load.py
@@ -4,9 +4,32 @@ import pytest
 
 
 @pytest.mark.run(order=2)
+def test_load_ephys():
+    assert len(morphs.paths.blocks()) > 0
+    block_path = morphs.paths.blocks()[-1]
+    assert morphs.paths.ACCURACIES_PKL.exists()
+    accuracies, cluster_accuracies = morphs.data.accuracies.load_cluster_accuracies()
+    cluster = cluster_accuracies[block_path].index[-1]
+    spikes = morphs.load.ephys_data(block_path, good_clusters=[cluster])
+    assert len(spikes) > 0
+
+
+@pytest.mark.run(order=2)
+def test_collapse_endpoints():
+    assert len(morphs.paths.blocks()) > 0
+    block_path = morphs.paths.blocks()[-1]
+    assert morphs.paths.ACCURACIES_PKL.exists()
+    accuracies, cluster_accuracies = morphs.data.accuracies.load_cluster_accuracies()
+    cluster = cluster_accuracies[block_path].index[-1]
+    spikes = morphs.load.ephys_data(block_path, good_clusters=[cluster],
+                                    collapse_endpoints=True)
+    assert len(spikes) > 0
+
+
+@pytest.mark.run(order=2)
 def test_shuffle_endpoints():
     assert len(morphs.paths.blocks()) > 0
-    block_path = morphs.paths.blocks()[0]
+    block_path = morphs.paths.blocks()[-1]
     assert morphs.paths.ACCURACIES_PKL.exists()
     accuracies, cluster_accuracies = morphs.data.accuracies.load_cluster_accuracies()
     cluster = cluster_accuracies[block_path].index[-1]
