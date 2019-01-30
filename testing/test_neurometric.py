6beb51f57c80a975fbd893d3592a4223dc1a5715
Marvin T <MarvinT@users.noreply.github.com>
2019-01-30 17:03:11 -0500

    test_combine_neuro_null_all

--- testing/test_neurometric.py
+++ testing/test_neurometric.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 import morphs
 import pytest
 from click.testing import CliRunner
+import pandas as pd
+import numpy as np
 
 
 @pytest.mark.run(order=2)
@@ -10,6 +12,18 @@ def test_load_generate_load_neurometric_null_all():
     assert not morphs.paths.num_shuffle_pkl(num_shuffles).exists()
     morphs.load.neurometric_null_all(num_shuffles)
     assert morphs.paths.num_shuffle_pkl(num_shuffles).exists()
+
+
+@pytest.mark.run(order=3)
+def test_combine_neuro_null_all():
+    num_shuffles = 8
+    all_samples_df = morphs.load.neurometric_null_all(num_shuffles)
+
+    nshuffle_dir = morphs.paths.num_shuffle_dir(num_shuffles).glob('*.pkl')
+    all_samples_df2 = pd.concat([morphs.utils.load._pickle(pkl_path)
+                                 for pkl_path in nshuffle_dir])
+    assert len(all_samples_df) == len(all_samples_df2)
+    assert np.all(np.sort(all_samples_df['errors']) == np.sort(all_samples_df2['errors']))
 
 
 @pytest.mark.run(order=3)
