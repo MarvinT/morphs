9deaed4a315e08407056b7c74120b477db50c4c5
Marvin T <MarvinT@users.noreply.github.com>
2019-02-04 18:08:37 -0500

    morphs.plot.cumulative_distribution

--- morphs/plot/__init__.py
+++ morphs/plot/__init__.py
@@ -3,4 +3,4 @@ import morphs.plot.psychometric
 import morphs.plot.neurometric
 import morphs.plot.singleunit
 import morphs.plot.spectrogram
-from morphs.plot.utils import savefig
+from morphs.plot.utils import savefig, cumulative_distribution
