77e20872cce7ef1e86ceb8674df85b6ab272e0c5
Marvin T <MarvinT@users.noreply.github.com>
2019-02-05 17:29:19 -0500

    Neurometric null dist notebook (#37)
    * null dist nb
    
    * update to pass on travis
    
    * morphs.plot.cumulative_distribution
    
    * refactor out plt_cum_dist

--- morphs/plot/utils.py
+++ morphs/plot/utils.py
@@ -1,4 +1,6 @@
 import morphs
+import numpy as np
+import matplotlib.pylab as plt
 
 
 def savefig(g, name, folder=morphs.paths.FIGURES_DIR, format=None,
@@ -10,3 +12,16 @@ def savefig(g, name, folder=morphs.paths.FIGURES_DIR, format=None,
     for format in formats:
         g.savefig((folder / (name + '.' + format)).as_posix(), format=format,
                   bbox_inches=bbox_inches, transparent=transparent, pad_inches=pad_inches)
+
+
+def cumulative_distribution(data, scaled=False, survival=False, label='Cumulative', **kwargs):
+    '''
+    plots cumulative (or survival) step distribution
+    '''
+    data = np.sort(data)
+    if survival:
+        data = data[::-1]
+    y = np.arange(data.size + 1, dtype=float)
+    if scaled:
+        y /= y[-1]
+    plt.step(np.concatenate([data, data[[-1]]]), y, label=label, **kwargs)
