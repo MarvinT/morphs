266dfd467b3bd47229360e975ff6deef990d69dd
Marvin T <MarvinT@users.noreply.github.com>
2019-04-10 15:36:14 -0400

    cleanup test_environment

--- testing/test_environment.py
+++ testing/test_environment.py
@@ -1,9 +1,5 @@
 from __future__ import absolute_import
-from __future__ import print_function
-import sys
 import pytest
-
-REQUIRED_PYTHON = "python"
 
 
 @pytest.mark.run(order=0)
@@ -12,5 +8,4 @@ def test_import():
 
 
 if __name__ == "__main__":
-    test_main()
     test_import()
