8c530616d755d85e46a7979de59ad3ce6fe2f644
Marvin T <MarvinT@users.noreply.github.com>
2019-04-17 12:14:08 -0400

    import utils first

--- morphs/plot/__init__.py
+++ morphs/plot/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
+from morphs.plot.utils import savefig, cumulative_distribution, morph_grid
 import morphs.plot.psychometric
 import morphs.plot.neurometric
 import morphs.plot.singleunit
 import morphs.plot.spectrogram
-from morphs.plot.utils import savefig, cumulative_distribution, morph_grid
