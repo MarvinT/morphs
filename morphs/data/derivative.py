9a3af665c5a99185a3d1fd1683796c84b1af3758
Marvin T <MarvinT@users.noreply.github.com>
2019-04-19 16:26:18 -0400

    import pd

--- morphs/data/derivative.py
+++ morphs/data/derivative.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import numpy as np
+import pandas as pd
 from scipy.integrate import quad
 from scipy.optimize import curve_fit
 import morphs
