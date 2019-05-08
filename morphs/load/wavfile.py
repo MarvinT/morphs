4056d7310d8da8d6b12ef25a54fefb0d9a0f38cb
Marvin T <MarvinT@users.noreply.github.com>
2019-05-08 13:29:22 -0400

    mkdir wavs parent

--- morphs/load/wavfile.py
+++ morphs/load/wavfile.py
@@ -3,6 +3,7 @@ import morphs
 
 
 def download_wavs():
+    morphs.paths.WAV_ZIP.parent.mkdir(parents=True, exist_ok=True)
     gdd.download_file_from_google_drive(
         file_id="1cHZsDqxiiM1uXJM6Yt7VJmaBMCb_PsgJ", dest_path=morphs.paths.WAV_ZIP.as_posix(), unzip=True
     )
