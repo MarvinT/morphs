a96bb18dc0bf68e5bfa0bec40da081548f32f44c
Marvin T <MarvinT@users.noreply.github.com>
2019-05-08 14:00:36 -0400

    explicit import of download_wavs

--- testing/test_wavs.py
+++ testing/test_wavs.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 import morphs
+from morphs.load.wavfile import download_wavs
 import pytest
 
 
 @pytest.mark.run(order=0)
 def test_download_wavs():
-    morphs.load.wavfile.download_wavs()
+    download_wavs()
     assert morphs.paths.WAV_DIR.exists()
