c1084c8a8335e2c42bcfb2fe9a61a1c4150122e3
Marvin T <MarvinT@users.noreply.github.com>
2019-05-18 15:41:25 -0400

    more refactoring

--- morphs/plot/utils.py
+++ morphs/plot/utils.py
@@ -159,17 +159,19 @@ def format_titles(g, upper=True, to_join=True):
         ax.set_title(title)
 
 
-def format_morph_dim_label(g, row_order, col_order, morph_dims, **kwargs):
+def format_morph_dim_label(g, row_order, col_order, morph_dims, flip=False, **kwargs):
     for row_index in range(len(row_order)):
         for col_index in range(len(col_order)):
             morph_dim = row_order[row_index] + col_order[col_index]
             if morph_dim in morph_dims:
+                if flip:
+                    morph_dim = morph_dim[::-1]
                 format_morph_dim_ax_label(
-                    g.axes[row_index, col_index], morph_dim, **kwargs
+                    g.axes[row_index, col_index], morph_dim=morph_dim, **kwargs
                 )
 
 
-def format_morph_dim_ax_label(ax, morph_dim, x_axis=True, divisions=4):
+def format_morph_dim_ax_label(ax, morph_dim="", x_axis=True, divisions=4):
     if x_axis:
         ax.set_xticks([1, 128])
         ax.set_xticklabels(morph_dim.upper())
